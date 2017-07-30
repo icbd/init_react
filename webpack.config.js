b29666fda84fa820fceb3317ad64b4a80b8f8a65
Baodong <wwwicbd@gmail.com>
2017-07-30 19:33:17 +0800

    fix
    如果出现 Cannot read property '0' of undefined, 删除.npm再重新安装

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const OpenBrowserPlugin = require("open-browser-webpack-plugin");
 
 module.exports = {
     entry: path.resolve(__dirname, "app/index.jsx"),
